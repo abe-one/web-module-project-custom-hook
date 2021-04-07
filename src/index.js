907328ee16b938b881d984879c35f7df2ccefd4c
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-07 15:59:12 -0500

    Build hook that returns value from local storage

--- src/index.js
+++ src/index.js
@@ -5,20 +5,31 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
-
 import "./styles.scss";
+
+// if not available set storage and stato
+// update state updates storage
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    return window.localStorage.getItem(key);
+  });
+  return [storedValue, setStoredValue];
+};
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
 
+  const [testRun, setTestRun] = useLocalStorage("testKey");
+
   useEffect(() => {
+    console.log(testRun);
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
