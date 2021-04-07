0363712bbaae82b605b99d58dbfc0f8214baa87b
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-07 16:19:34 -0500

    Build out useLocalStorage hook

--- src/index.js
+++ src/index.js
@@ -7,22 +7,37 @@ import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
-// if not available set storage and stato
 // update state updates storage
+// Parse JSON?
 const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
-    return window.localStorage.getItem(key);
+    if (window.localStorage.getItem(key)) {
+      return window.localStorage.getItem(key);
+      // If the key exists in localStorage then set the key's value to state
+    } else {
+      window.localStorage.setItem(key, JSON.stringify(initialValue));
+      return initialValue;
+      // If not save the key:initialValue to storage and set to state
+    } //storedValue state
   });
-  return [storedValue, setStoredValue];
+
+  const setLocalStoredValue = (storedValue) => {
+    window.localStorage.setItem(key, JSON.stringify(storedValue));
+    setStoredValue(storedValue);
+  };
+
+  return [storedValue, setLocalStoredValue];
 };
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
 
-  const [testRun, setTestRun] = useLocalStorage("testKey");
+  const [testRun, setTestRun] = useLocalStorage("nothing", "I added this");
 
   useEffect(() => {
+    console.log(testRun);
+    setTestRun("Changed it?");
     console.log(testRun);
     axios
       .get(
