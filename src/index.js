2fad059d087357922193cba2346417960db2239f
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-07 18:00:55 -0500

    Persist dark mode

--- src/index.js
+++ src/index.js
@@ -46,6 +46,7 @@ const App = () => {
   const [bodyDark, setBodyDark] = useDarkMode(false);
 
   useEffect(() => {
+    setBodyDark(JSON.parse(window.localStorage.getItem("localDark")));
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
