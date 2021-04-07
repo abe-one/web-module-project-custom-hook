f05043dbe8bbc25fa02941edbb5d41976730109d
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-07 17:06:40 -0500

    Build bodyDarkMode Hook

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
-// update state updates storage
-// Parse JSON?
 const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     if (window.localStorage.getItem(key)) {
@@ -27,30 +25,38 @@ const useLocalStorage = (key, initialValue) => {
   };
 
   return [storedValue, setLocalStoredValue];
+}; //useLocalStorage
+
+const useDarkMode = (initialBoolean) => {
+  const [darkMode, setDarkMode] = useState(initialBoolean);
+
+  const setBodyDarkMode = (newBoolean) => {
+    setDarkMode(newBoolean);
+    if (newBoolean) {
+      return document.querySelector("body").classList.add("dark-mode");
+    }
+    return document.querySelector("body").classList.remove("dark-mode");
+  };
+  return [darkMode, setBodyDarkMode];
 };
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
-
-  const [testRun, setTestRun] = useLocalStorage("edit", {
-    me: "I added this",
-  });
+  const [bodyDark, setBodyDark] = useDarkMode(false);
 
   useEffect(() => {
-    console.log(testRun);
-    setTestRun("Changed it?");
-    console.log(testRun);
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
       .then((res) => setCoinData(res.data))
       .catch((err) => console.log(err));
-  }, [testRun]);
+  }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+      <button onClick={(_) => setBodyDark(!bodyDark)}>Body Dark</button>
       <Charts coinData={coinData} />
     </div>
   );
